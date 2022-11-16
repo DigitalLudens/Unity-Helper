d091fc1cdda65f0a76ac3633caeaad55f82434e2
HQ\alex.oh <alez.oh@nsuslab.com>
2022-11-16 10:32:49 +0900

    헬퍼들 추가

---
+++ Runtime/GameObjectHelper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace beio.Helper
+{
+    public static partial class GameObjectHelper
+    {
+        public static void Destory(this GameObject obj)
+        {
+            if (Application.isPlaying) GameObject.Destroy(obj);
+            else GameObject.DestroyImmediate(obj);
+        }
+        public static void SetActive(this Behaviour behaviour, bool active)
+        {
+            if (behaviour == null) return;
+            behaviour.gameObject.SetActive(active);
+        }
+    }
+}
